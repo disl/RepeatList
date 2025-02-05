aa6b481c42d318b82a0e7185cd2bbb7f969e4f67
Dimitri Sluzki <dimitri.sluzki@haas.de>
2025-02-05 17:01:46 +0100

    5.2.25  17:00

---
+++ RepeatList/Themes/DarkTheme.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RepeatList.Themes
+{
+    public partial class DarkTheme : ResourceDictionary
+    {
+        public DarkTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RepeatList/Themes/LightTheme.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RepeatList.Themes
+{
+    public partial class LightTheme : ResourceDictionary
+    {
+        public LightTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
