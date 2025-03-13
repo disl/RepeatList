a7bd5d58fe8e938b913ac5aee4d4c75b78d14875
Dimitri Sluzki <dimitri.sluzki@haas.de>
2025-03-13 16:49:53 +0100

    13.03.25  16:49

---
+++ RepeatList/Models/PickerItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatList.Models
+{
+    public class PickerItem
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; } // Pfad zur Bilddatei
+    }
+}
